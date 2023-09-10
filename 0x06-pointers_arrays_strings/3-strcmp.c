3f3472ecbc8a45457748050a18f1b205b1b8b87b
hawetengg <hawetengirma277@gmail.com>
2023-09-10 13:14:39 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strcmp - A function that compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ * Return: An integer value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int flag = 0, flag2;
+	int i, res;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+	if (s1[i] != s2[i])
+	{
+	flag = 1;
+	flag2 = i;
+	break;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+	break;
+	}
+	}
+	if (flag == 1)
+	{
+	res = s2[flag2] - s1[flag2];
+	}
+	else
+	res = 0;
+	return (res);
+}
