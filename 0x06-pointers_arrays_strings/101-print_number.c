d31b70216d6e8a2232b11f58cbf6a6b6e22f77e7
hawetengg <hawetengirma277@gmail.com>
2023-09-19 03:46:55 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - A function that prints an integer.
+ * @n: The number to be printed
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,26 +5,26 @@
  * @str: A pointer that holds the adress of the string
  * Return: Capitalized string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ',' ||
-		str[i] == ';' ||
-		str[i] == '.' ||
-		str[i] == '!' ||
-		str[i] == '?' ||
-		str[i] == '"' ||
-		str[i] == '(' ||
-		str[i] == ')' ||
-		str[i] == '{' ||
-		str[i] == '}')
+		if (str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}')
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			if (str[i] >= 97 && str[i] <= 122)
 			{
-				str[i + 1] -= 32;
+				str[i] -= 32;
 			}
 		}
 		i++;
