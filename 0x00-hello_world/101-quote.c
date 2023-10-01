2a54b3c09198ce3faf1651176ffad85247818e74
hawetengg <hawetengirma277@gmail.com>
2023-10-01 01:14:09 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A program that prints a string to the standard output
+ * return: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return(1);
+}
