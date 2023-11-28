e76581bbe69c078099cb3ec8e22cd49dd78c6149
hawetengg <hawetngirma277@gmail.com>
2023-11-27 17:47:19 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	int nletters;
-	int rwr;
+	int file, result = 1, countn = 0;
 
 	if (!filename)
 		return (-1);
-
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file_d == -1)
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-	if (!text_content)
-		text_content = "";
-	for (nletters = 0; text_content(nletters); nletters++)
-		rwr = write(file_d, text_content, nletters);
-
-	if (rwr == -1)
+	if (text_content)
+	{
+		while (text_content[countn])
+			countn++;
+		result = write(file, text_content, countn);
+	}
+	if (result == -1)
 		return (-1);
-	close(file_d);
-
+	close(file);
 	return (1);
 }
