44fd7a01c3907fa8ac703b4519c6aa51b08adfb8
hawetengg <hawetngirma277@gmail.com>
2023-11-27 18:34:21 -0800

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *str);
-
+void error_100(int f0, char *buffer);
+void error_99(int f0, char *buffer, char *argv);
+void error_98(int f0, char *buffer, char *argv);
 #endif
